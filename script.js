e1387aa22666e96c20db330f4adc3120c0bf8e9e
Jaime Harman <62770805+jaimeharman@users.noreply.github.com>
2020-07-14 17:03:14 -0400

    made changes to README.md

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var currentQuestion = 0; 
-var score = 0; 
- 
+var currentQuestion = 0;
+var score = 0;
+
 //Select elements from HTML
 var quizContainer = document.getElementById('quizContainer');
 var resultsContainer = document.getElementById('resultsContainer');
@@ -10,7 +10,7 @@ var opt2 = document.getElementById('opt2');
 var opt3 = document.getElementById('opt3');
 var opt4 = document.getElementById('opt4');
 var nextButton = document.getElementById('nextButton');
-var scoreContainer = document.getElementById('scoreContainer'); 
+var scoreContainer = document.getElementById('scoreContainer');
 var timer = document.getElementById('timer');
 
 //Questions
@@ -46,55 +46,53 @@ var totQuestions = questions.length;
 
 //Timer starts at 30 seconds and counts down; 
 var secondsLeft = 30;
-function startTimer () {
-    var timerInterval = setInterval(function() {
+function startTimer() {
+    var timerInterval = setInterval(function () {
         secondsLeft--;
         timer.textContent = secondsLeft;
-    if(secondsLeft === 0) { 
-        clearInterval(timerInterval); 
-        timer.textContent = ("Time's up!") 
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval);
+            timer.textContent = ("Time's up!")
         }
-    // else if (finishQuiz) {
-    //   clearInterval(timerInterval) 
-    // }
-    }, 1000); 
-      }
+    }, 1000);
+}
 
 
 //Question is loaded and the options are displayed for user to select from
-function loadQuestion (questionIndex) {
-     var q = questions[questionIndex]; 
+function loadQuestion(questionIndex) {
+    var q = questions[questionIndex];
     questionEl.textContent = (questionIndex + 1) + '.' + q.question;
-    opt1.textContent = q.option1; 
+    opt1.textContent = q.option1;
     opt2.textContent = q.option2;
     opt3.textContent = q.option3
     opt4.textContent = q.option4;
 };
 //User can only select from options given and if user does not answer, alert will pop up asking user to answer question
-function loadNextQuestion () {
+function loadNextQuestion() {
     var selectedOption = document.querySelector('input[type=radio]:checked');
-    if(!selectedOption){
+    if (!selectedOption) {
         alert('Please select your answer!');
-        return; 
+        return;
     }
-//If the correct answer is selected, score will increase
-    var answer = selectedOption.value; 
-    if(questions[currentQuestion].answer === answer){
-        score+=5; 
+    //If the correct answer is selected, score will increase
+    var answer = selectedOption.value;
+    if (questions[currentQuestion].answer === answer) {
+        score += 5;
     }
-//If question is wrong, no score will be added and user will click next button to continue the quiz
-    selectedOption.checked = false; 
-    currentQuestion++; 
-    if(currentQuestion == totQuestions -1){
+    //If question is wrong, no score will be added and user will click next button to continue the quiz
+    selectedOption.checked = false;
+    currentQuestion++;
+    if (currentQuestion == totQuestions - 1) {
         nextButton.textContent = 'Finish';
     }
-//At the end of the quiz, results will be displayed for user
-    if(currentQuestion == totQuestions){
+    //At the end of the quiz, results will be displayed for user
+    if (currentQuestion == totQuestions) {
         quizContainer.style.display = 'none';
         resultsContainer.style.display = '';
-        scoreContainer.textContent = 'Your Score:' + score; 
-        return; 
+        scoreContainer.textContent = 'Your Score:' + score;
+        return;
     }
+    
     loadQuestion(currentQuestion);
 }
 
@@ -103,36 +101,36 @@ function submitInitials() {
     var name = document.getElementById('inputField').value;
     console.log(name);
 
-//Set name to local storage
-localStorage.setItem('initials', name);
+    //Set name to local storage
+    localStorage.setItem('initials', name);
 
-//Pull information from local storage and append to scoreOne, scoreTwo, scoreThree
-var highScoreArray = [];
-var scoreObj = {
-    name: name,
-    score: score
-}
-//storageScore is all the information in local storage
-var storageScore = JSON.parse(window.localStorage.getItem('highScore'));
-console.log(storageScore)
+    //Pull information from local storage and append to scoreOne, scoreTwo, scoreThree
+    var highScoreArray = [];
+    var scoreObj = {
+        name: name,
+        score: score
+    }
+    //storageScore is all the information in local storage
+    var storageScore = JSON.parse(window.localStorage.getItem('highScore'));
+    console.log(storageScore)
 
-//iterate through storageScore and add information to highScoreArray
-for ( var i = 0; i < storageScore.length; i++) {
-    highScoreArray.push(storageScore[i])
-}
-//Add new score to highScoreArray
-highScoreArray.push(scoreObj);
+    //iterate through storageScore and add information to highScoreArray
+    for (var i = 0; i < storageScore.length; i++) {
+        highScoreArray.push(storageScore[i])
+    }
+    //Add new score to highScoreArray
+    highScoreArray.push(scoreObj);
 
-//Push all scores into local storage
-localStorage.setItem("highScore", JSON.stringify(highScoreArray));
+    //Push all scores into local storage
+    localStorage.setItem("highScore", JSON.stringify(highScoreArray));
 
-var totalStorageScore = JSON.parse(window.localStorage.getItem('highScore'));
-console.log(totalStorageScore)
+    var totalStorageScore = JSON.parse(window.localStorage.getItem('highScore'));
+    console.log(totalStorageScore)
 
-//Target score Id and set it's HTML to local storage objects name and score
-document.getElementById("scoreOne").innerHTML = totalStorageScore[0].name + " " + totalStorageScore[0].score;
-document.getElementById("scoreTwo").innerHTML = totalStorageScore[1].name + " " + totalStorageScore[1].score;
-document.getElementById("scoreThree").innerHTML = totalStorageScore[2].name + " " + totalStorageScore[2].score;
+    //Target score Id and set it's HTML to local storage objects name and score
+    document.getElementById("scoreOne").innerHTML = totalStorageScore[0].name + " " + totalStorageScore[0].score;
+    document.getElementById("scoreTwo").innerHTML = totalStorageScore[1].name + " " + totalStorageScore[1].score;
+    document.getElementById("scoreThree").innerHTML = totalStorageScore[2].name + " " + totalStorageScore[2].score;
 
 }
 
